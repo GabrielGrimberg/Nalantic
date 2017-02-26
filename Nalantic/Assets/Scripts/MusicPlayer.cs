61570312940a50bd9cee4a8707e98e62a5bc5780
Gabriel Grimberg <gabby.gr97@hotmail.com>
2017-02-26 22:22:14 +0000

    Collision and Trigger

--- Nalantic/Assets/Scripts/MusicPlayer.cs
+++ Nalantic/Assets/Scripts/MusicPlayer.cs
@@ -2,17 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MusicPlayer : MonoBehaviour {
+public class MusicPlayer : MonoBehaviour 
+{
+	//No multiple music playing.
+	static MusicPlayer instance = null;
+
+	void Awake()
+	{
+		if(instance != null)
+		{
+			Destroy(gameObject);
+		}
+		else
+		{
+			instance = this;
+			//Takes a GameObject instance and doesn't destroy on load.
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+
+	}
 
 	// Use this for initialization
 	void Start () 
 	{
-		//Takes a GameObject instance and doesn't destroy on load.
-		GameObject.DontDestroyOnLoad(gameObject);
+		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		
 	}
 }
