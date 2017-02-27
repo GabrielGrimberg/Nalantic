0ac261fb3b10f6d69c03c445548b7a06e0694120
Gabriel Grimberg <gabby.gr97@hotmail.com>
2017-02-27 18:42:49 +0000

    Mouse Interaction

---
+++ Nalantic/Assets/Scripts/Paddle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//Struct of x,y,z
+		Vector3 paddlePos = new Vector3(0.5f, this.transform.position.y ,0f);
+
+		float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
+
+		//Clmap used to make the object not go out of bounds.
+		paddlePos.x = Mathf.Clamp(mousePosInBlocks, 0.5f, 15.5f);
+
+		this.transform.position = paddlePos;
+		
+	}
+}
