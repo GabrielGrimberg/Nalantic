237cccb0ac6a89ad8345d3ee8da553e3f8c275a1
Gabriel Grimberg <gabby.gr97@hotmail.com>
2017-03-12 21:39:57 +0000

    Sprite Sheet, Bricks Changing On Hit

--- Nalantic/Assets/Scripts/Brick.cs
+++ Nalantic/Assets/Scripts/Brick.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Brick : MonoBehaviour 
 {
 	public int maxHits;
+	public Sprite[] hitSprites;
 	private int timesHit;
 	private LevelManager levelManager;
 
@@ -31,6 +32,18 @@ public class Brick : MonoBehaviour
 		{	
 			Destroy(gameObject);
 		}
+		else
+		{
+			LoadSprites();
+
+		}
+	}
+
+	void LoadSprites()
+	{
+		int spriteIndex = timesHit - 1;
+		this.GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+
 	}
 
 	//TODO Remove this method once we can actually win.
